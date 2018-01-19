66daa8b52b7cea446392fd568e7361604882811f
sasatani <sasayabaku@yahoo.co.jp>
2018-01-19 23:28:29 +0900

    add function which return top3 probability and label in `face_predict`

--- Learning/predict_face.py
+++ Learning/predict_face.py
@@ -5,8 +5,20 @@ from keras.layers.convolutional import Conv2D
 from keras.layers.pooling import MaxPool2D
 import cv2
 import numpy as np
+import pandas as pd
 
 def face_predict(img):
+
+    """
+    画像からラベルを予測
+
+    img: face image
+
+    label: star's label
+    proba: all star probability
+    max_indexes: top3 indexes
+    max_proba: top3 probability
+    """
 
     # Resize image to size of input layer
     img = cv2.resize(img, (100, 100))
@@ -18,19 +30,26 @@ def face_predict(img):
     model.load_weights('cnn_model_weight.hdf5')
 
     label = model.predict_classes(target)
-    proba = model.predict_proba(target)
+    proba = model.predict_proba(target)[0]
 
-    return label, proba
+    # Pick the TOP3 similar class index
+    max_indexes = np.argpartition(-proba, 3)[:3]
+
+    max_proba_num = pd.DataFrame([proba[x] for x in max_indexes]).round(3)
+    max_proba = max_proba_num.values
+
+    return label, proba, max_indexes, max_proba
 
 
 if __name__ == '__main__':
 
-    img_file = './facedata/26/6.jpg'
+    img_file = './facedata/37/6.jpg'
 
     img = cv2.imread(img_file)
 
-    label, proba = face_predict(img)
+    label, proba, top_3, top_3_proba = face_predict(img)
 
-    print(proba)
-    print(proba.shape)
     print(label)
+    print(proba)
+    print(top_3)
+    print(top_3_proba)
