24face400cfa9ac72302c51dec27c59e60b5df66
sasatani <sasayabaku@yahoo.co.jp>
2018-01-29 23:43:38 +0900

    Create age-gender-estimate API

--- API/predict.py
+++ API/predict.py
@@ -3,7 +3,9 @@
 # Add PATH of Project Directory
 import os
 import sys
+
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir)))
+
 
 import flask
 from flask_cors import CORS, cross_origin
@@ -12,12 +14,14 @@ from Learning.predict_face import face_predict, face_extract
 import cv2
 import pandas as pd
 
+
 app = flask.Flask(__name__)
 CORS(app)
 
 Host_name = "172.21.39.178"
 
 stars = pd.read_csv("list.csv", header=None)
+
 
 @app.route("/predict")
 def predict():
@@ -47,5 +51,24 @@ def predict():
         "name": name_list
     })
 
+
+@app.route("/age_gender_predict")
+def age_gender_predict():
+
+    """
+    API that returns predicted age and gender from facial image
+    :return: json (predicted age, predicted gender)
+    """
+
+    face_file = "./face.jpg"
+    face = cv2.imread(face_file)
+    gender, age = age_gender_predict(face)
+
+    return flask.jsonify({
+        "gender": gender,
+        "age": age
+    })
+
+
 if __name__ == "__main__":
-    app.run(host=Host_name, port=5000 ,debug=True)
+    app.run(host=Host_name, port=5000, debug=True)
